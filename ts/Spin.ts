056c3b1cf9b50a90947e5433d6fdb6d5f4f70ab1
Mateusz <mateusz.suder@awesome.ais>
2021-02-03 14:20:30 +0100

    some tries

--- ts/Spin.ts
+++ ts/Spin.ts
@@ -26,7 +26,7 @@ export class Spin {
 	}
 
 	drawSymbols() {
-		for(let i = 0; i < 5; i++) {
+		for(let i = 0; i < LEVELS[this.stage].length; i++) {
 			this.resultBoard[i]= [];
 			for(let j = 0; j < LEVELS[this.stage][i]; j++) {
 				this.resultBoard[i][j] = this.drawSymbol();
@@ -35,10 +35,16 @@ export class Spin {
 	}
 
 	checkForWinnings(): Winnings[] {
+		console.log(this.resultBoard);
 		const result: Winnings[] = [];
+		const test: any[] = [];
 		for(let i = 0; i < LEVELS[this.stage][0]; i++) {
-			
-			break;
+			const first = [ this.resultBoard[0][i] ];
+			for(let j = 1; j < LEVELS[this.stage].length; j++) {
+				for(let k = 0; k < LEVELS[this.stage][j]; k++) {
+					const third = [ ...first, this.resultBoard[j][k] ];
+				}
+			}
 		}
 		return [];
 	}
